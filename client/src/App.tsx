20d9696ecf9bae8a9ce97274e6466f30e5e19a42
Aman Kumar <akjha1469@gmail.com>
2023-11-21 19:28:31 +0530

    update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -47,7 +47,7 @@ function App() {
     setTransactionInProgress(true);
     const payload = {
       type: "entry_function_payload",
-      function: `${moduleAddress}::counter::get_count`,
+      function: `${moduleAddress}::myCounter::increment`,
       type_arguments: [],
       arguments: [],
     };
